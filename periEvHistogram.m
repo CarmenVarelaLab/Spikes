00f5df50449583a2f6405eef4f7ac2897a1720c1
Varela Lab <haberlashailas@gmail.com>
2022-03-13 20:30:24 -0400

    Update periEvHistogram.m

--- periEvHistogram.m
+++ periEvHistogram.m
@@ -1,11 +1,13 @@
 function [PETH, h] = periEvHistogram(spiketimes,evTimestamps,width,bin)
 % calculate the average peri-event histogram in spikes/sec given the spike
-% times and event times (provide event timestamps in a column vector) 
+% times and event times  
 % width -- determines the x-axis of the histogram (from - width to  + width)
 % bin -- bin size
 
+
 edges = -width:bin:width;
 evTimestamps= reshape(evTimestamps,length(evTimestamps),1);  % column vector
+spiketimes= reshape(spiketimes,length(spiketimes),1);  
 
 % calculate average histogram
 %%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -19,8 +21,9 @@ for i= 1:size(evTimestamps,1)
     PeakTriggSpks{i,:}= temp1;
     periEvHist(i,:)= histcounts(PeakTriggSpks{i,:}',edges);      
 end
-% average across K-complexes in spks/s
+% average across events in spks/s
 PETH = mean(periEvHist,1)./bin;   
+% PETH = sum(periEvHist,1);
 
 %%   PLOT
 scrsz=get(0,'screensize'); 
@@ -32,8 +35,8 @@ for n= 1:size(evTimestamps,1)
     spikesd= PeakTriggSpks{n,:}';
     x= repmat(spikesd,2,1);
     sx=size(x,2);
-    y1 = cs+0.5 *(ones(1,sx));
-    y2= cs-0.5 *(ones(1,sx));
+    y1 = cs+ 0.5 *(ones(1,sx)); % change for larger tick marks, e.g., + 1.5
+    y2= cs- 0.5 *(ones(1,sx)); % change for larger tick marks
     y3= NaN(1,sx);
     y= [y1;y2;y3];
     notnum= NaN(1,sx);
@@ -43,20 +46,25 @@ for n= 1:size(evTimestamps,1)
     y = reshape(y,1,sss(1)*sss(2));
     subplot(2,1,1);
     hold on;
-    plot(x,y,'k','linewidth',1);
+    plot(x,y,'k','linewidth',1); % change for thicker tick marks
     cs=cs+1;
 end
-xlabel('Time (s)'); ylabel('Event number');
+
+% figure; subplot(2,1,1); imagesc(t,1:size(psthSpks,1),psthSpks);
+% colormap(1-gray);
+
+xlabel('Time (s)'); ylabel('Event number'); ylim([0 size(evTimestamps,1)]);
 set(gca,'linewidth',1.5);
 set(gca,'TickDir','out');
-set(gca,'fontsize',12); 
+set(gca,'fontsize',10); 
 
 subplot(2,1,2);  % histogram
-plot(edges(1:end-1)+(bin./2),(PETH),'linewidth',1.5); 
+b = bar(edges(1:end-1)+(bin./2),(PETH),1); 
+% plot(edges(1:end-1)+(bin./2),(PETH),'linewidth',1.5); 
+set(b,'FaceColor','k','EdgeColor','k');
 xlabel('Time (s)'); ylabel('Spikes/s');
 set(gcf, 'color', 'white'); 
 set(gca,'linewidth',1.5);
 set(gca,'TickDir','out');
-set(gca,'fontsize',12); 
-
+set(gca,'fontsize',10); 
 
